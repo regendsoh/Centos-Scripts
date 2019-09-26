bfd662beb63d05212e0818dd383b7cef579bf2ff
Regend Soh <17229500+regendsoh@users.noreply.github.com>
2019-09-26 17:44:08 +0800

    Installation of latest kernel

---
+++ kernel.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+yum update -y && yum upgrade -y
+rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+yum --enablerepo=elrepo-kernel install kernel-ml -y
+egrep ^menuentry /etc/grub2.cfg | cut -f 2 -d \'
+grub2-set-default 0
+reboot
