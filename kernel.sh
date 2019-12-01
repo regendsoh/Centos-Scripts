3ba865081a73fed0cbc23efc36a241844eeb78d0
Regend Soh <17229500+regendsoh@users.noreply.github.com>
2019-12-02 06:55:14 +0800

    Remove old kernel

--- kernel.sh
+++ kernel.sh
@@ -5,4 +5,5 @@ rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
 yum --enablerepo=elrepo-kernel install kernel-ml -y
 egrep ^menuentry /etc/grub2.cfg | cut -f 2 -d \'
 grub2-set-default 0
+package-cleanup --oldkernels --count=1 -y
 reboot
